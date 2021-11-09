2db4be1684fac2e8679f3f2b647f5a4c7f37a12d
Jessica Chou <j4chou@gmail.com>
2021-11-08 21:41:21 -0800

    Add config files for lint/format

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'plugin:react/recommended',
+		'standard'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: 'module'
+	},
+	plugins: [
+		'react',
+		'@typescript-eslint'
+	],
+	rules: {
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': ['error'],
+		'react/jsx-uses-react': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'brace-style': [
+			'error',
+			'stroustrup'
+		],
+		'no-tabs': [
+			'error',
+			{
+				allowIndentationTabs: true
+			}
+		],
+		indent: [
+			'error',
+			'tab'
+		],
+		semi: [
+			'error',
+			'always'
+		]
+	}
+};
