60612b8c7c531e32989bbfea39e5e22cb5827809
Jessica Chou <j4chou@gmail.com>
2021-11-08 21:41:01 -0800

    Add components to display homes

---
+++ src/components/HouseList/HouseList.tsx
@@ -0,0 +1,53 @@
+import styles from './HouseList.module.css';
+import ListItem from '../ListItem/ListItem';
+
+import { _1 } from '../../constants/index';
+
+interface House {
+	id: number,
+	address: string,
+	homeowner: string,
+	price: number,
+	photoURL: string
+}
+
+interface HouseListProp {
+	houses: House[],
+	lastHouseRef: (node: HTMLLIElement) => HTMLLIElement | void
+}
+
+const HouseList = ({
+	houses,
+	lastHouseRef
+}: HouseListProp) => (
+	<ul className={styles.list}>
+		{houses.map(
+			({ id, address, homeowner, price, photoURL }, index) => {
+				const isLastHouse = houses.length - _1 === index;
+				if (isLastHouse) {
+					return (
+						<ListItem
+							key={id}
+							address={address}
+							homeowner={homeowner}
+							lastHouseRef={lastHouseRef}
+							price={price}
+							photoURL={photoURL}
+						/>
+					);
+				}
+				return (
+					<ListItem
+						key={id}
+						address={address}
+						homeowner={homeowner}
+						price={price}
+						photoURL={photoURL}
+					/>
+				);
+			}
+		)}
+	</ul>
+);
+
+export default HouseList;
---
+++ src/components/ListItem/ListItem.tsx
@@ -0,0 +1,44 @@
+import styles from './ListItem.module.css';
+
+interface ListItemProp {
+	key: number,
+	address: string,
+	homeowner: string,
+	price: number,
+	photoURL: string,
+	lastHouseRef?: (node: HTMLLIElement) => HTMLLIElement | void,
+}
+
+interface Attr {
+	ref?: (node: HTMLLIElement) => HTMLLIElement | void
+}
+
+const ListItem = ({
+	address,
+	homeowner,
+	lastHouseRef,
+	price,
+	photoURL
+}: ListItemProp) => {
+	const email = `${homeowner.replace(' ', '.')}@gmail.com`;
+	const attributes:Attr = {};
+	if (lastHouseRef) {
+		attributes.ref = lastHouseRef;
+	}
+	return (
+		<li className={styles.listItem} {...attributes}>
+			<img className={styles.house} alt={`${address}`} src={photoURL} width="100%" height="auto" />
+			<div className={styles.details}>
+				<h1 className={styles.price}>
+					${price.toLocaleString()}
+				</h1>
+				<div className={styles.description}>
+					<p>{address}</p>
+					<p>Contact <a href={`mailto:${email}`}>{homeowner}</a></p>
+				</div>
+			</div>
+		</li>
+	);
+};
+
+export default ListItem;
