5977b57b68b099712cc38887c4a14cafd20b3bb3
Jessica Chou <j4chou@gmail.com>
2021-11-08 21:40:48 -0800

    Add toast component

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,14 @@
+import styles from './Toast.module.css';
+
+interface ToastProps {
+	success: boolean,
+	message?: string
+}
+
+const Toast = ({ success, message = 'Sorry, an error has occurred. Please try again.' }: ToastProps) => (
+	<div className={success ? styles.success : styles.error}>
+		<p>{message}</p>
+	</div>
+);
+
+export default Toast;
